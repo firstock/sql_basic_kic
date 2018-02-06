879c70e00f0f987ac9295459395d9777b0128609
firstock <optimist20@naver.com>
2018-02-06 21:07:47 +0900

    학원 sql예제 ch2 1st problem-이래도 짤리고 저래도 짤리고

--- example_ch2_function.sql
+++ example_ch2_function.sql
@@ -2,5 +2,8 @@
 select * from student;
 --
 select name||'의 키는 '||height||' cm, 몸무게는 '||weight||' kg 입니다'
-    as "학생의 키와 몸무게"
+    as "학생의 키와 몸무게            " --응? 짧으면 짤리고 여기서 길면 에러
+    from student;
+    
+select name, height, weight
     from student;
