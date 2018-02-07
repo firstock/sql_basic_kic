bcaa4a97bd1d3cf0afcdc7c2dd6795ec3102fe87
firstock <optimist20@naver.com>
2018-02-07 20:34:19 +0900

    학원 function수업,기본 예제 다풀음

--- example_ch2_function.sql
+++ example_ch2_function.sql
@@ -7,3 +7,154 @@ select name||'의 키는 '||height||' cm, 몸무게는 '||weight||' kg 입니다'
     
 select name, height, weight
     from student;
+    
+select name,'의 키는 ',height,' cm,몸무게는 ',weight,'kg 입니다'
+    from student;
+--    
+--[실습]
+
+--문제1) EMP 테이블에서 scott의 사원번호, 성명, 담당업무(소문자로), 담당업무(대문자로), 첫 글자만 대문자로 변환하여 출력하여라.
+select * from emp;
+select empno, ename, lower(job), initcap(job)
+    from emp;
+--문제2) EMP 테이블에서 이름의 첫 글자가 ‘K’보다 큰 사원의 사원번호, 이름, 업무를 출력하여라.
+select empno, ename, job
+    from EMP
+    where upper(substr(ename,1,1)) > 'K';
+--문제3) EMP 테이블에서 이름이 6자리 이상인 사원의 이름과 업무를 출력하여라.
+select ename, job
+    from emp
+    where length(ename) >= 6;
+--문제4) EMP 테이블에서 이름 중 ‘L’자의 첫 위치를 출력하여라.
+select ename, decode(instr(ename,'L',1),0,'없어',instr(ename,'L',1)) as "L 첫 위치"
+    from emp;
+    
+--[실습]
+
+--문제1) 이름에 *를 왼쪽에 채워 모두 동일한 15개의 이름으로 변환하고 업무와 급여를 출력한다.
+select lpad(ename,15,'*'), job, sal
+    from emp;
+
+--문제2) 급여를 모두 동일하게 만단위로 보여지게 변환하고 이름과 부서번호를 함께 출력한다.
+select trim(to_char(sal,'999,999,999,999')), ename, deptno
+    from emp;
+--문제3) EMP 테이블에서 담당 업무 중 좌측에 ‘A’를 삭제하고 급여 중 좌측의 1(숫자)을 삭제하여 출력하여라.
+select job, sal from emp;
+select ltrim(job,'A'), ltrim(sal,1)
+    from emp;
+--문제4) EMP 테이블에서 담당 업무 중 우측에 ‘T’를 삭제하고 급여 중 우측의 0을 삭제하여 출력하여라.
+select rtrim(job,'T'), rtrim(sal,0)
+    from emp;
+--문제5) EMP 테이블에서 JOB에 ‘A’를 ‘$’로 바꾸어 출력하여라.
+select translate(job,'A','$')
+    from emp;
+--[ 실습 ]
+
+--문제1) EMP 테이블에서 현재까지 근무일수가 몇 주, 몇 일인가를 출력하여라.  단, 근무일수가 많은 사람 순으로 출력하여라.
+select * from emp;
+select sysdate-hiredate as 날짜기준, months_between(sysdate, hiredate) as 월기준  -- ver 0.7
+    from emp
+    order by months_between(sysdate, hiredate) desc
+    ;
+    
+    -- 나머지 연산? mod
+select floor((sysdate-hiredate)/7)||'주 '||mod(floor(sysdate-hiredate),7)||'일'   -- 답은 맞는데, 안배운 함수잖아. 더 간단한게 있을겨
+    from emp
+    order by months_between(sysdate, hiredate) desc
+    ;
+    
+select trunc(123.65), floor(123.65) from dual;
+select trunc(-123.65), floor(-123.65) from dual;
+--문제2) EMP 테이블에서 10번 부서원의 현재까지의 근무 월수를 계산하여 출력하여라. 
+desc emp;
+select * from emp;
+select trunc(months_between(sysdate, hiredate)) "10번부서 근무월수"
+    from emp
+    where 10= deptno;
+    
+--문제3) EMP 테이블에서 입사한 달의 남은 근무 일수를 계산하여 출력하여라. 단, 토요일과 일요일도 근무일수에 포함한다. (첫달 월급정산용+주휴?)
+select hiredate,last_day(hiredate),last_day(hiredate)-hiredate+1 "당일포함 남은근무일수"
+    from emp;
+    
+select hiredate
+    from emp;
+--문제4) EMP 테이블에서 10번 부서원의 입사 일자로부터 돌아오는 금요일을 계산하여 출력하여라.
+alter session set nls_language= korea;
+select hiredate, next_day(hiredate,'금')
+    from emp;
+
+--[ 실습 ] 
+
+--문제1) EMP 테이블에서 10번 부서원의 입사 일자를 '1 MAY 1981'와 '1998년 1월 1일' 의 형태로 각각 출력하여라.
+alter session set nls_language= english;
+select ltrim(to_char(hiredate,'dd MON rrrr'),0), translate(to_char(hiredate,'rrrr"년" mm"월" dd"일"'), ' 0', ' ')
+    from emp;
+--문제2) EMP 테이블에서 20번 부서원의 급여 앞에 $를 삽입하고 3자리마다 ,를 출력하여라.
+select trim(to_char(sal,'$999,999,999,999'))
+    from emp
+    where 20= deptno;
+--문제3) february 22, 1981에 입사한 사원의 이름, 업무, 입사일자를 출력하여라. nls_date_language=american일 경우
+alter session set nls_language="english";
+select * from emp;
+select ename, job, hiredate
+    from emp
+    where to_date(substr('febraury 22,1981',1,3)||substr('febraury 22,1981',-8),'month dd,yyyy')= hiredate;
+    
+select to_date('febraury 22,1981','month dd,yyyy') from dual;
+select 'feb221981' from dual;
+select to_date('feb221981','monthddyyyy') from dual;
+select substr('febraury 22,1981',1,3)||substr('febraury 22,1981',-8) from dual;
+
+--[ 실습 ]
+
+--문제1) EMP 테이블에서 부서번호가 10이면 ‘A’로, 20이면 ‘B’로, 30이면 ‘C’로 바꾼 뒤 이름, 업무, 부서번호와 함께 출력하여라.
+desc emp;
+select deptno, decode(deptno,10,'A', 20,'B', 30,'C')
+    from emp;
+--문제2) EMP 테이블에서 JOB이 ANALYST이면 급여 증가는 10%이고,  CLERK이면 15%, MANAGER이면 20%이고, 다른 업무에 대해서는 급여 증가가 없다. 
+--       사원번호, 이름, 업무, 급여, 증가된 급여를 출력하여라.
+select empno, ename, job, sal, decode(lower(job),'analyst',sal*1.1, 'clerk',sal*1.15, 'manager',sal*1.2) "급여증가"
+    from emp;
+--문제3) EMP 테이블의 사원이름, 매니저번호(MGR)를 출력하고, 매니저번호가 null이면 ‘상위관리자’로 표시하고, 매니저번호가 있으면 ‘7869담당‘으로 표시하여라.
+select * from emp;
+select ename, mgr, decode(mgr,null,'상위관리자','7869담당')
+    from emp;
+
+--[ 실습 ] 
+------------ 이하는 아직 수업 안 나간부분 '18.1.7.수/ 더 좋은 코드가 있을겨!!
+
+--문제1) emp 테이블을 이용하여 각 부서별로 사원수, 최대연봉, 최소연봉, 평균연봉을 구하고 급여평균으로 정렬하여라.
+nope> select sal
+    from emp
+    order by avg(sal);
+
+desc emp; -- empno: not null
+select deptno "부서", count(empno) "사원수", max(sal) "최대연봉", min(sal) "최소연봉", floor(avg(sal)) "평균연봉"
+    from emp
+    group by deptno
+    order by avg(sal) desc;
+--문제2) emp 테이블을 이용하여 각 업무별로 사원수, 급여평균을 구하고 업무별로 정렬하여라.
+select job "업무", count(empno) "사원수", floor(avg(sal)) "급여평균"
+    from emp
+    group by job
+    order by job
+    ;
+--문제3) emp 테이블을 이용하여 업무별(JOB), 부서별(DEPTNO)로 그룹화하여 각각의 인원수와 급여평균을 구하고 급여평균이 많은 순으로 정렬하여라.
+select job from emp
+union
+select name from student;
+
+select job "업무별", deptno "부서별", count(empno) "각 인원수", floor(avg(sal)) "급여평균"   -- ???? 뭔 의미?? 아마 맞게 한듯? 
+    from emp
+    group by job, deptno
+    order by avg(sal) desc;
+--문제4) EMP 테이블에서 부서 인원이 4명보다 많은 부서의 부서번호, 인원수, 급여의 합을 출력하여라.
+select deptno "부서번호", count(empno), sum(sal)        -- ?? 아마 맞게 했을 걸
+    from emp
+    group by deptno
+    having count(empno) >4;
+--문제5) EMP 테이블에서 업무별 급여의 평균이 3000 이상인 업무에 대해서 업무명, 평균 급여, 급여의 합을 구하여라.
+select job "업무명", floor(avg(sal)) "평균 급여", sum(sal) "급여의 합"     -- ?? 아마 맞겠지
+    from emp
+    group by job
+    having 3000 <= avg(sal);
--- function_basic.sql
+++ function_basic.sql
@@ -64,3 +64,216 @@ select name,
     from student
     where '서진수' =name
     ;
+    
+    
+    
+-- instr
+select * from emp;
+select instr(empno,'l') as return from emp; -- ????
+
+select * from student;
+select tel, instr(tel,')',1,1) as ")의 위치"
+    from student;
+    
+--    지역번호만 추출하기
+select tel, substr(tel,1,instr(tel,')')-1) as "지역번호"
+    from student;
+    
+--    찾는 a는 맨끝이 아니라, 뒤에서 3번째인 5
+select 'ababada', instr('ababada','a',-2,1)
+    from dual;
+    
+----    실습문제: instr
+select * from emp;
+select  empno, ename, lower(job), initcap(job)
+    from emp;
+   
+--   emp테이블에서 이름의 첫 글자가 'K'보다 큰 사원의 사원번호, 이름, 업무 조회
+select empno, ename, job
+    from emp
+    where 'K' < substr(ename,1,1);
+    
+--    emp에서 이름이 6자리 이상인 사원의 이름,업무
+select ename,job
+    from emp
+    where ename like '______%'; -- % 아무거나 에는 공백도 포함
+    
+    --다른 방법
+select ename,job
+    from emp
+    where 6<= length(ename);
+--    
+select ename, instr(ename,'L',1,1) as "이름중 L자 첫 위치"
+    from emp;
+    
+    
+-- lpad
+select lpad('123456789',10,'*') as return from dual;
+select lpad('123456',3,'*') as return from dual;
+select lpad('123456',8) as return from dual;
+
+select length(lpad('123456',8)) as return from dual;
+
+select rpad('123456',8,'*') as return from dual;
+
+-- trim
+select ltrim('aababa','a'),
+        rtrim('aababa','a')
+    from dual;
+    
+select trim('   abdef  d   ')
+    from dual;
+    
+create table test_trim
+(
+    col1    char(5),
+    col2    varchar2(5)
+);
+
+insert into test_trim values('test','test');
+commit;
+
+select *
+    from test_trim
+    where col1= col2;
+    
+select * from test_trim;
+select length(col1), length(col2) from test_trim;
+
+select *
+    from test_trim
+    where trim(col1)= trim(col2);
+    
+desc test_trim;
+
+-- replace
+select replace ('1234','12','ab') as return from dual;
+select replace('123412','12','ab') as return from dual; -- 다 바꿈
+
+select * from student;
+-- 두번째 글자를 #으로 바꾸기
+select replace(name, substr(name,2,1) ,'#')
+    from student
+    ;
+
+    -- 뒤 7자리를 전부 *로 바꾸고 싶은데..
+select replace(jumin, substr(jumin,7,7) ,'*******')
+    from student
+    ;
+    -- 뒤에 오는게 7자리인지 모를땐? ex_주소
+nope> select replace(jumin, rpad(substr(jumin,7),13,'*')) --원본그대로 나옴
+    from student
+    ;
+
+nope> select replace(jumin, *, '*')
+    from student
+    where jumin like '_______%';
+    
+select replace(jumin, substr(jumin,8,6) ,'******')
+    from student
+    ;
+    
+--    translate
+select translate('1234','12','ab') as return from dual;
+select translate('123421','123','ab') as return from dual;
+
+select replace('abcba','abc','AB'), translate('abcba','abc','AB') from dual;
+
+select * from professor;
+
+select id, replace(id,'-','')
+    from professor
+    where id like '%-%';
+
+select id, translate(id,'-','')  -- 얜 왜 다 날라감??
+    from professor
+    where id like '%-%';
+
+select id, translate(id,'any-','any')
+    from professor
+    where id like '%-%';
+    
+    
+--    sysdate
+select sysdate+10
+    from dual;
+    
+select floor(months_between(sysdate, '2016/10/22'))
+    from dual;
+    
+select sysdate, hiredate, trunc(months_between(sysdate, hiredate))
+    from emp;
+    
+select add_months(sysdate,3) as "3개월후"
+    , add_months(sysdate,-3) as "3개월전"
+    from dual;
+    
+select sysdate, next_day(sysdate,'금') as "다음 요일"
+    from dual;
+    
+    -- 그냥은 안 됨
+select next_day(sysdate,'FRI') as "다음 요일"
+    from dual;
+    -- 영어로 찾으려면 세션이 날짜 저장하는 방식을 영어로 바꿔야
+alter session set nls_date_language= american;
+select next_day(sysdate,'FRI') as "다음 요일"
+    from dual;
+    -- 대소 구분 X
+select next_day(sysdate,'fri') as "다음 요일" 
+    from dual;
+    
+    -- 지금 설정값? 계정 system
+select *
+    from v$parameter
+    where name='nls_date_language';
+    
+alter session set nls_date_language= korean;
+
+---------- to_char는 양이 많아서 파일 분리
+
+---- null . p50
+-- nvl
+select sal, comm, nvl(comm,0)
+    from emp;
+    
+select sal, comm, sal+comm, nvl(comm,0)
+    from emp;
+
+    -- 바람직
+select sal, comm, sal+nvl(comm,0)
+    from emp;
+    
+select hpage, nvl(hpage,'1')
+    from prefessor;
+    
+select hpage, nvl(hpage,1) -- ㄴㄴ
+    from prefessor;
+    
+-- nvl2
+select sal, comm, nvl2(comm, '널 아냐','내안에 널있어')
+    from emp;
+    
+select sal, comm, nvl2(comm, comm*1.2, 0)
+    from emp;
+    
+-- decode. 조건  ? T : F
+select decode('10','10','십','노십')
+    from dual;
+    
+    --    jumin 1남 2여
+select * from student;
+select substr(jumin,7,1)
+        , decode(substr(jumin,7,1),1, '남','여')
+    from student;
+    
+    
+    -- 부서번호 10총무 2재무 3인사
+select * from emp;
+select decode(deptno,10,'총무',   20,'재무',   30,'인사')
+    from emp;
+    
+select * from emp;
+select decode(deptno,10,'총무',   20,'재무',   30,'인사', 'NA')
+    from emp;
+    
+    
